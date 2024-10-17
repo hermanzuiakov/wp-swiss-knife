7f74adf27a80152260cb25e4dceae8bfb2cf06e2
Herman Zuiakov <herman.zuiakov@gmail.com>
2024-10-17 12:48:44 +0200

    Create base file && Add gitignore

--- index.php
+++ index.php
@@ -1,1 +1,3 @@
 <?php
+
+die('Access denied');
--- wp-swiss-knife.php
+++ wp-swiss-knife.php
@@ -1,1 +1,29 @@
 <?php
+/**
+ *
+ * WP Swiss Knife
+ *
+ * @package WP Swiss Knife
+ * @author Herman Zuiakov
+ * @license GPL-2.0+
+ * @version 1.0.0
+ *
+ *
+ * @wordpress-plugin
+ * Plugin Name: WP Swiss Knife
+ * Plugin URI: https://your-plugin-url.com
+ * Description: A powerful developer's toolkit plugin for WordPress.
+ * Version: 1.0.0
+ * Author: Herman Zuiakov
+ * Author URI: https://zuiakovwebdev.website
+ * Update URI: false
+ * License: GPLv2 or later
+ * Text Domain: wp-swiss-knife
+ * Domain Path: /languages
+ * Requires PHP: 7.4
+ * Requires at least: 6.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
